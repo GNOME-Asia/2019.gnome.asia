1df3bf713e5a61b73ce017562df1795c1eb37dcd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-30 11:18:23 +0000

    Deploy GNOME-Asia/2019.gnome.asia to github.com/GNOME-Asia/2019.gnome.asia.git:gh-pages

---
+++ precache-manifest.3bf1212685491edecb59ea2eebf21a86.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1d012722f8d9fdc53983d19c3092df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee1d032711acfaa9226",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a8c9c3dbdcf580aa7826",
+    "url": "/static/css/main.f5ebe042.chunk.css"
+  },
+  {
+    "revision": "3ee1d032711acfaa9226",
+    "url": "/static/js/2.1f874b7e.chunk.js"
+  },
+  {
+    "revision": "a8c9c3dbdcf580aa7826",
+    "url": "/static/js/main.2d10d8f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "083763abcc6ab29f3f56290eeeb4148c",
+    "url": "/static/media/KLiM.083763ab.png"
+  },
+  {
+    "revision": "a84fa528569631adf467da5e8b6c181d",
+    "url": "/static/media/P1160835.a84fa528.JPG"
+  },
+  {
+    "revision": "87af656da3b2bd1206217508b4366724",
+    "url": "/static/media/gimpscape.87af656d.png"
+  },
+  {
+    "revision": "038b31fcad832adabd3221d5111712cb",
+    "url": "/static/media/gnome-id.038b31fc.png"
+  },
+  {
+    "revision": "6c54e1e3a447f4cc6abb513187b62238",
+    "url": "/static/media/gresikdev.6c54e1e3.png"
+  },
+  {
+    "revision": "99c6630d0c7f9203022ffe6b9337831d",
+    "url": "/static/media/kabarlinux.99c6630d.png"
+  },
+  {
+    "revision": "0fedb69d901dc442a41342f37f0a790c",
+    "url": "/static/media/pesona.0fedb69d.png"
+  },
+  {
+    "revision": "3aa14a62ef4b0071ab4f3dc1b95b73de",
+    "url": "/static/media/photo_2019-07-10 19.01.08.3aa14a62.jpeg"
+  },
+  {
+    "revision": "5c8aa4e1b9179ef659f1196c0bdb93b4",
+    "url": "/static/media/sakti.5c8aa4e1.png"
+  },
+  {
+    "revision": "8b61e43a59537bca540d0483d2eb2cbd",
+    "url": "/static/media/santika.8b61e43a.png"
+  },
+  {
+    "revision": "feabe4a96280add3d10a210b70734a56",
+    "url": "/static/media/saptanawa.feabe4a9.jpg"
+  },
+  {
+    "revision": "ea5657351a3ecf87ff097ab16cf425bf",
+    "url": "/static/media/unmuh.ea565735.png"
+  }
+]);
