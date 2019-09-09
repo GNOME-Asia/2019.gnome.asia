69d8d5a5cf4e0173bf7a3f6ba6dda173c373f2aa
Muhammad Firdaus <mbex71@gmail.com>
2019-09-09 20:59:28 +0700

    change env

--- src/Settings/env.js
+++ src/Settings/env.js
@@ -1,5 +1,5 @@
-const environtment = 'local';
-// const environtment = 'production';
+// const environtment = 'local';
+const environtment = 'production';
 
 const local = "http://localhost:8000/api"
 const production = "https://kenproject.id/api"
