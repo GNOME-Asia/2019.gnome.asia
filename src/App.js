7a27136e45cac1eca6d75841e38847285c3aadbd
Rania Amina <reaamina@gmail.com>
2019-09-10 07:43:42 +0700

    remove unused variable

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Staff from './Containers/Staff';
 import Media from './Containers/Media';
 import Community from './Containers/Community';
 import Proposal from './Containers/Proposal';
-import Speakers from './Containers/Speakers';
+// import Speakers from './Containers/Speakers';
 import Loading from './Components/Loading';
 import { 
   BrowserRouter as Router
