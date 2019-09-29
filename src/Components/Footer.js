b237c3f354cd0d5c24ad350e9cfafefa7f6cdbd3
Rania Amina <reaamina@gmail.com>
2019-09-30 02:49:39 +0700

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component{
                 <div className="row justify-contents-bottom align-content-bottom">
                     <div className="col-md-8 col-12">
                         <h6 style={{ fontWeight:'600' }}>GNOME Event Code of Conduct</h6>
-                        <div>The GNOME Events Code of Conduct applies to all GNOME events, including hackfests, release parties, workshops and other small events. Each GNOME conference should have a tailored version of this Code of Conduct, which contains additional details about how to report incidents and get support. <br/><a href="https://wiki.gnome.org/CodeOfConduct/Events">See Details...</a></div>
+                        <div>For your convenience, kindly read for further understanding on <br/><a href="https://gnome.id/en/gnome-event-code-of-conduct/">GNOME Event Code of Conduct.</a></div>
                     </div>
                     <div className="col-md-4 col-12 text-left text-md-right mt-md-0 mt-3">
                         <h6 style={{ fontWeight:'600' }}>Contact Us</h6>
